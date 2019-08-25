c0e13d3a42e0e41e9dcedff88294ae692b076528
bert hubert <bert.hubert@netherlabs.nl>
2019-08-26 00:17:41 +0200

    glonass special sign support

--- bits.hh
+++ bits.hh
@@ -3,5 +3,6 @@
 
 unsigned int getbitu(const unsigned char *buff, int pos, int len);
 int getbits(const unsigned char *buff, int pos, int len);
+int getbitsglonass(const unsigned char *buff, int pos, int len);
 void setbitu(unsigned char *buff, int pos, int len, unsigned int data);
 unsigned int rtk_crc24q(const unsigned char *buff, int len);
