dc531ca4a6288cd87793572a7da3d4051454b1b8
bert hubert <bert.hubert@netherlabs.nl>
2020-07-29 11:12:04 +0200

    make rinjoin use the IOD field

--- rinjoin.cc
+++ rinjoin.cc
@@ -9,6 +9,7 @@ struct Value
   optional<int> af0Inav;
   optional<int> af0Fnav;
   int af1;
+  int iod;
   optional<int> BGDE1E5a;
   optional<int> BGDE1E5b;
 };
@@ -30,6 +31,7 @@ int main(int argc, char** argv)
         s.af1 = rint(ldexp(e.af1,46));
         s.BGDE1E5a = rint(ldexp(e.BGDE1E5a,32));
         s.BGDE1E5b = rint(ldexp(e.BGDE1E5b,32));
+        s.iod = e.iodnav;
       }
       else {
         s.af0Fnav = rint(ldexp(e.af0,34));
@@ -41,10 +43,10 @@ int main(int argc, char** argv)
     }
     
   }
-  cout<<"timestamp sv af0fnav af0inav af1 bgde1e5a bgde1e5b\n";
+  cout<<"timestamp sv iod af0fnav af0inav af1 bgde1e5a bgde1e5b\n";
   for(const auto& s : satmap) {
     if(s.second.af0Fnav.has_value() && s.second.af0Inav.has_value() && s.second.BGDE1E5a.has_value() && s.second.BGDE1E5b.has_value())
-    cout << s.first.first<<" " <<s.first.second<<" " <<
+      cout << s.first.first<<" " <<s.first.second<<" " << s.second.iod<<" "<<
       *s.second.af0Fnav << " " << *s.second.af0Inav <<" " << s.second.af1<<" " <<*s.second.BGDE1E5a <<" " << *s.second.BGDE1E5b << "\n";
   }
 }
